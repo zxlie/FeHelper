cbf56bcd8637a202eec43e23eedbb29a9a3c5c06
zxlie <xianliezhao@foxmail.com>
2018-04-12 11:55:32 +0800

    优化build脚本

--- chrome/build.js
+++ chrome/build.js
@@ -24,17 +24,27 @@ let copyFiles = function () {
 
 // js 文件压缩
 let jsCompress = function () {
+    console.log('> Javascript文件压缩 ...')
     console.time('> Javascript文件压缩');
+
     glob.sync('output/apps/**/*.js').map(cf => {
+        // 添加例外，有些js文件不用压缩
+        let exclude = /\/ga\.js|\/(codemirror|syntaxhighlighter|uglifyjs3)\/|.*\.min\.js|\/vue\.js/;
+        if (exclude.test(cf)) {
+            return false;
+        }
+
+        let start = new Date();
         let rawJs = fs.readFileSync(cf, 'utf8').toString();
         let compressedJs = jsMinifier(rawJs);
         if (compressedJs.code) {
             fs.writeFileSync(cf, compressedJs.code);
+            console.log('\t文件压缩成功，', ('耗时' + (new Date() - start) + 'ms').padEnd(12, ' '), cf);
         } else {
-            console.log('---------------------', cf, '----------------------');
-            console.log(compressedJs.error.message, '\n')
+            console.log('\t文件压缩失败：', cf);
         }
     });
+
     console.timeEnd('> Javascript文件压缩');
 };
 
@@ -72,29 +82,27 @@ let htmlCompress = function () {
     console.timeEnd('> HTML文件压缩');
 };
 
-// manifest文件压缩
-let manifestCompress = function () {
+// zip the fehelper
+let zipForChromeWebStore = function () {
+
     console.time('> Manifest文件压缩');
     let mf = './output/apps/manifest.json';
     let json = require(mf);
     fs.writeFileSync(mf, JSON.stringify(json));
     console.timeEnd('> Manifest文件压缩');
-};
 
-// zip the fehelper
-let zipForChromeWebStore = function () {
     console.time('> FeHelper打包');
     shell.cd(`${rootPath}/output`);
     shell.exec('zip -r fehelper.zip apps/ > /dev/null');
     console.timeEnd('> FeHelper打包');
     console.log('\n\n================================================================================');
+    console.log('    当前版本：', json.version);
     console.log('    去Chrome商店发布吧：https://chrome.google.com/webstore/devconsole');
     console.log('================================================================================\n\n');
 };
 
 /************************build start*****************************/
 copyFiles();
-manifestCompress();
 cssCompress();
 htmlCompress();
 jsCompress();
